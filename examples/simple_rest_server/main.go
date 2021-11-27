24dc156fd27649ed1ca15988b1e8e65eeae410e4
RGood <mullan17@gmail.com>
2021-11-27 17:59:02 -0500

    Put code in package and create simple example

--- examples/simple_rest_server/main.go
+++ examples/simple_rest_server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import controller "github.com/rgood/go-controllers"
+import (
+	"net/http"
+
+	"github.com/rgood/go-controllers/pkg/controller"
+)
+
+type Response struct {
+	Message string
+}
 
 func main() {
-	cb := controller.NewControllerBuilder()
+	cb := controller.NewControllerbuilder()
+
+	http.HandleFunc(
+		"/",
+		cb.Handle(http.MethodGet, func(req *http.Request) (int, interface{}) {
+			return 200, "Hello world!"
+		}).Create(),
+	)
+
+	http.ListenAndServe(":8080", nil)
 }
